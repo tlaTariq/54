68d25c1e54410e8fe46a5e5665b85def705a818e
Talha <tla.tariq1995@gmail.com>
2017-07-25 02:07:20 +0500

    first

---
+++ EADProject/Controllers/HomeController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EADProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Songs()
+        {
+            return View();
+        }
+        public ActionResult Rock()
+        {
+            return View();
+        }
+
+        public ActionResult Pop()
+        {
+            return View();
+        }
+
+        public ActionResult Jazz()
+        {
+            return View();
+        }
+
+        public ActionResult MyPL()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[ActionName("Login")]
+        [HttpPost]
+        public ActionResult Login2()
+        {
+            String login = Request["txtLogin"];
+            String password = Request["txtPassword"];
+
+            var dto = Models.DBHelper.Validate(login, password);
+            if (dto != null)
+            {
+                Session["Login"] = login;
+                Session["ID"] = dto.UserID;
+
+                if (login == "admin")
+                    Session["admin"] = "yes";
+                else
+                    Session["user"] = "yes";
+
+                return Redirect("~/Home/Index");
+            }
+            else
+            {
+                ViewBag.Login = login;
+                ViewBag.Msg = "Not valid";
+            }
+
+            return View("Login");
+        }
+
+        public ActionResult Signup()
+        {
+            String login = Request["txtLogin"];
+            String password = Request["txtPassword"];
+
+            if (Models.DBHelper.ValidateSignUp(login, password))
+            {
+                Session["Login"] = login;
+                Session["ID"] = Models.DBHelper.addUser(login, password);
+
+                if (login == "admin")
+                    Session["admin"] = true;
+                else
+                    Session["admin"] = true;
+
+                return Redirect("~/Home/Index");
+            }
+            else
+            {
+                ViewBag.Login = login;
+                ViewBag.Msg = "Username already registered!";
+            }
+
+            return View("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["Login"] = null;
+            Session.Abandon();
+
+            return Redirect("~/Home/Index");
+        }
+
+        public ActionResult Add()
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add2(string txtName, string txtSinger, string txtCategory, HttpPostedFile file)
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            String name = txtName;
+            String singer = txtSinger;
+            String category = txtCategory;
+            var song = Request.Files.Get("file");
+                 
+            var allowedExtensions = new[] { ".mp3" };
+            var checkextension = Path.GetExtension(song.FileName).ToLower();
+
+            if (!allowedExtensions.Contains(checkextension))
+            {
+                TempData["notice"] = "PLEASE SELECT \"mp3\" FILE";
+                return Redirect("Add");
+            }
+
+
+
+            if (song != null && song.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(song.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/songs"), fileName);
+                song.SaveAs(path);
+
+                Models.DBHelper.addSong(name, singer, category, fileName);
+            }
+
+            return View("Songs");
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            Models.SongsDTO obj = Models.DBHelper.getSongByID(id);
+
+            ViewBag.id = obj.SongID;
+            ViewBag.singer = obj.Singer;
+            ViewBag.name = obj.Name;
+
+            return View();
+        }
+
+        public ActionResult Edit2()
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            int id = Convert.ToInt32(Request["txtID"]);
+            String name = Request["txtName"];
+            String singer = Request["txtSinger"];
+
+            Models.DBHelper.updateSong(id, name, singer);
+            
+            return View("Songs");
+        }
+
+        public ActionResult AddtoPlaylist(int id)
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            int uid = Convert.ToInt32(Session["ID"]);
+
+            Models.DBHelper.addUserSong(uid, id);
+            
+            return View("Songs");
+        }
+
+        public ActionResult RemoveFromPlaylist(int id)
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            int uid = Convert.ToInt32(Session["ID"]);
+
+            Models.DBHelper.removeUserSong(uid, id);
+
+            return View("MyPL");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (Session["Login"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            Models.SongsDTO obj = Models.DBHelper.getSongByID(id);
+
+            ViewBag.link = obj.Link;
+            String link = obj.Link;
+
+            var path = Path.Combine(Server.MapPath("~/Content/songs"), link);
+            FileInfo f = new FileInfo(path);
+            f.Delete();
+
+
+            Models.DBHelper.deleteSong(id);
+
+            return View("Songs");
+        }
+    }
+}
---
+++ EADProject/Models/DBHelper.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EADProject.Models
+{
+    public static class DBHelper
+    {
+
+        private static String connString = @"Data Source=DESKTOP-ERLET40\SQLEXPRESS2016;Initial Catalog=MusicStore;Integrated Security=True";
+
+
+        public static UsersDTO Validate(String Login, String Password)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Users where UserName = '{0}' and Password = '{1}'", Login, Password);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    if (reader.Read() == true)
+                    {
+                        UsersDTO dto = new UsersDTO();
+                        dto.UserID = reader.GetInt32(0);
+                        dto.UserName = reader.GetString(1);
+                        dto.Password = reader.GetString(2);
+
+                        return dto;
+                    }
+
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static Boolean ValidateSignUp(String Login, String Password)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Users");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    while (reader.Read() == true)
+                    {
+                         string UserName = reader.GetString(1);
+                        if (Login == UserName)
+                            return false;
+                        
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static List<SongsDTO> getAllSongs()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Songs");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    List<SongsDTO> list = new List<SongsDTO>();
+
+                    while (reader.Read() == true)
+                    {
+                        SongsDTO dto = new SongsDTO();
+                        dto.SongID = reader.GetInt32(0);
+                        dto.Name = reader.GetString(1);
+                        dto.Singer = reader.GetString(2);
+                        dto.Link = reader.GetString(3);
+
+                        list.Add(dto);
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static List<SongsDTO> getRockSongs()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Songs Where Category like '{0}'", "Rock");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    List<SongsDTO> list = new List<SongsDTO>();
+
+                    while (reader.Read() == true)
+                    {
+                        SongsDTO dto = new SongsDTO();
+                        dto.SongID = reader.GetInt32(0);
+                        dto.Name = reader.GetString(1);
+                        dto.Singer = reader.GetString(2);
+                        dto.Link = reader.GetString(3);
+
+                        list.Add(dto);
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static List<SongsDTO> getPopSongs()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Songs Where Category like '{0}'", "Pop");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    List<SongsDTO> list = new List<SongsDTO>();
+
+                    while (reader.Read() == true)
+                    {
+                        SongsDTO dto = new SongsDTO();
+                        dto.SongID = reader.GetInt32(0);
+                        dto.Name = reader.GetString(1);
+                        dto.Singer = reader.GetString(2);
+                        dto.Link = reader.GetString(3);
+
+                        list.Add(dto);
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static List<SongsDTO> getJazzSongs()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Songs Where Category like '{0}'", "Jazz");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    List<SongsDTO> list = new List<SongsDTO>();
+
+                    while (reader.Read() == true)
+                    {
+                        SongsDTO dto = new SongsDTO();
+                        dto.SongID = reader.GetInt32(0);
+                        dto.Name = reader.GetString(1);
+                        dto.Singer = reader.GetString(2);
+                        dto.Link = reader.GetString(3);
+
+                        list.Add(dto);
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static List<SongsDTO> getPlaylistSongs(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select s.SongID from Songs s join UserSong us on s.SongID = us.SongID  join Users u on u.UserID = us.UserID");
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    List<SongsDTO> list = new List<SongsDTO>();
+
+                    while (reader.Read() == true)
+                    {
+                        SongsDTO dto = new SongsDTO();
+                        int Songid = reader.GetInt32(0);
+
+                        dto = getSongByID(Songid);
+
+                        list.Add(dto);
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static SongsDTO getSongByID(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Select * from Songs where SongID = {0}", id);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    SongsDTO dto = new SongsDTO();
+
+                    if (reader.Read() == true)
+                    {
+                        dto.SongID = reader.GetInt32(0);
+                        dto.Name = reader.GetString(1);
+                        dto.Singer = reader.GetString(2);
+                        dto.Link = reader.GetString(3);                        
+                    }
+
+                    return dto;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void updateSong(int id, string name, string singer)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Update Songs set Name = '{0}', Singer = '{1}' where SongID = {2}", name, singer, id);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        public static void deleteSong(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Delete from Songs where SongID = {0}", id);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        public static void addSong(string name, string singer, string category, string link)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Insert into Songs (Name, Singer, Link, Category) values('{0}', '{1}', '{2}', '{3}')", name, singer, link, category);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+        }
+
+        public static int addUser(string login, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Insert into Users (UserName, Password) values('{0}', '{1}')", login, password);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                    sqlQuery = String.Format("Select UserID from Users where UserName = '{0}'", login);
+                    comm = new SqlCommand(sqlQuery, conn);
+                    SqlDataReader reader = comm.ExecuteReader();
+
+                    int id = -1;
+                    if (reader.Read() == true)
+                    {
+                        id = reader.GetInt32(0);
+                    }
+
+                    return id;
+
+                }
+                catch (Exception ex)
+                {
+                    return -1;
+                }
+            }
+
+        }
+
+
+        public static void addUserSong(int uid, int sid)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Insert into UserSong (UserID, SongID) values({0}, {1})", uid, sid);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+        }
+
+        public static void removeUserSong(int uid, int sid)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    String sqlQuery = String.Format("Delete from UserSong Where UserID = {0} and SongID = {1}", uid, sid);
+                    SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                    comm.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+        }
+    }
+}
